fb57f1f189275abee6667e88b1fbf86bd1e33afe
fasilofficial <fasilofficialofficial@gmail.com>
2023-10-18 23:11:41 +0530

    Modified files in views/user to access partials

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_LOCAL_URI);
-    console.log(`Database running on: mongodb://${mongoose.connection.host}/${mongoose.connection.name}`);
+    // await mongoose.connect(process.env.MONGODB_LOCAL_URI);
+    // console.log(`Database running on: mongodb://${mongoose.connection.host}/${mongoose.connection.name}`);
 
-    // await mongoose.connect(process.env.MONGODB_CLOUD_URI);
-    // console.log(`Database running on: mongodb+srv://${mongoose.connection.host}/${mongoose.connection.name}`);
+    await mongoose.connect(process.env.MONGODB_CLOUD_URI);
+    console.log(`Database running on: mongodb+srv://${mongoose.connection.host}/${mongoose.connection.name}`);
   } catch (error) {
     console.log(error);
   }
