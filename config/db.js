03538eec82f72284c9004e78b301575bd30fbe8e
fasilofficial <fasilofficialofficial@gmail.com>
2023-11-15 16:26:08 +0530

    Bugs fixed

--- config/db.js
+++ config/db.js
@@ -4,11 +4,8 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_LOCAL_URI);
-    console.log(`Database running on: mongodb://${mongoose.connection.host}/${mongoose.connection.name}`);
-
-    // await mongoose.connect(process.env.MONGODB_CLOUD_URI);
-    // console.log(`Database running on: mongodb+srv://${mongoose.connection.host}/${mongoose.connection.name}`);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database connected");
   } catch (error) {
     console.log(error);
   }
